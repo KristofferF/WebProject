9da77e26294937c7e3bbaabbe02f68dfba89bca7
Kristoffer Freiholtz <Kristoffer.Freiholtz@hotmail.com>
2015-09-02 01:46:48 +0200

    Updated

--- js/js-normal.js
+++ js/js-normal.js
@@ -1,3 +1,31 @@
 function AlertEmptyProject() {
     alert("Sorry this project is currently unavailable");
 }
+
+function clickMe(){
+    var number = Math.floor(Math.random() * 100) + 0;
+    var answer = prompt("Guess a number between 0-100");
+    if (answer != null && (answer >= 0 || answer <= 100)) {
+
+        document.getElementById("demo").innerHTML =
+            "Hello " + person + "! How are you today?";
+    }
+    //var lightbox = document.getElementById("lightbox"),
+    //    dimmer = document.createElement("div");
+    //
+    //dimmer.style.width =  window.innerWidth + 'px';
+    //dimmer.style.height = window.innerHeight + 'px';
+    //dimmer.className = 'dimmer';
+    //
+    //dimmer.onclick = function(){
+    //    document.body.removeChild(this);
+    //    lightbox.style.visibility = 'hidden';
+    //}
+    //
+    //document.body.appendChild(dimmer);
+    //
+    //lightbox.style.visibility = 'visible';
+    //lightbox.style.top = window.innerHeight/2 - 50 + 'px';
+    //lightbox.style.left = window.innerWidth/2 - 100 + 'px';
+    //return false;
+}
